040a570aa9720ace877e314d8a18eb7957c99a88
zahrahafezi <zahrahm2004@gmail.com>
2024-01-31 04:30:39 +0330

    essential functions
    functions we need in views file

--- myproject/BookAppointment/views.py
+++ myproject/BookAppointment/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# for staff:
+# update_profile
+# view_appointment
+# Update_Clinic_Info
+# send_notification
+# cancel_appointment
+
+# for user:
+# update_profile
+# book_appointment
+# cancel_appointment
+# update_booking_day
+# update_booking_time
+# send_notification
+# rate_clinic
